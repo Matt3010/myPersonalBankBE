88f21717efd8f8426825b7c1c64a5111ec63c5ae
FG420 <facchinatog97@gmail.com>
2023-09-21 10:05:22 +0200

    [NPWI-3]
    nome files errato

---
+++ src/api/transaction-type/transaction-type.model.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+import { TransactionType as iTransactionType } from "./transaction-type.entity";
+
+const transactionTypeSchema: Schema<iTransactionType> = new Schema({
+    name: String, type: String
+});
+
+transactionTypeSchema.set("toJSON", {
+    virtuals: true,
+    transform: (_, ret) => {
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+    },
+});
+
+export const TransactionType = model<iTransactionType>("TransactionType", transactionTypeSchema);
