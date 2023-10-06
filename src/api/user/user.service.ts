0d2a497be9f26973fc066d5b65699f5d910770dc
Matteo Trombin <matteotromb@gmail.com>
2023-10-06 07:35:10 +0200

    Error

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -6,7 +6,7 @@ import { User as UserModel } from "./user.model";
 
 export class UserService {
 
-  async add(user: User, credentials: {email: string, password: string}): Promise<User | { message : string }> {
+  async add(user: User, credentials: {email: string, password: string}): Promise<User | object> {
     const existingIdentity = await UserIdentityModel.findOne({'credentials.email': credentials.email});
     if (existingIdentity) {
       return {
